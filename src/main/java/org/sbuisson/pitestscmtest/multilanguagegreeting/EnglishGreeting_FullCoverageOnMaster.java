87c15edace6b607332e37481ecd05c7d52d72022
sbuisson <sbuisson@xebia.fr>
2017-10-07 13:10:37 +0200

    update

--- src/main/java/org/sbuisson/pitestscmtest/multilanguagegreeting/EnglishGreeting_FullCoverageOnMaster.java
+++ src/main/java/org/sbuisson/pitestscmtest/multilanguagegreeting/EnglishGreeting_FullCoverageOnMaster.java
@@ -17,10 +17,11 @@ public class EnglishGreeting_FullCoverageOnMaster extends Greeting {
         char firstChar = who.charAt(0);
         String message;
         if (Character.isUpperCase(firstChar)) {
-            message = "Hello, " + who;
+            message = "Hello, ";
         } else {
-            message = "hy, " + who;
+            message = "hy, ";
         }
+        message += who;
         return message;
     }
 }
